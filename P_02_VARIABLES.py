b6f96fe1fa2c314f488ce1c771fd75425476d620
flip2561 <renniel3270@masseyhigh.school.nz>
2024-10-29 12:26:56 +1300

    base comments

---
+++ P_02_VARIABLES.py
@@ -0,0 +1,20 @@
+# Code to learn about variables
+
+greeting = "hello world"
+
+# Variables can be a number
+fave_num =7
+num_string = "5"
+
+print(fave_num * 2)
+print(num_string * 5)
+
+# Contstants are uppercase
+PI = 3.14
+
+
+a_01_my_num = 7 
+
+
+
+
