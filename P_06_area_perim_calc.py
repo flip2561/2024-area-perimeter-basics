2608e678bc7ae958c222129ad04dff20b7401d42
flip2561 <renniel3270@masseyhigh.school.nz>
2024-11-25 12:20:34 +1300

    Add files via upload

---
+++ P_06_area_perim_calc.py
@@ -0,0 +1,13 @@
+width = float(input("width:"))
+height = float(input("Height:  "))
+
+area = width * height
+perimeter = 2 * (width + height)
+
+print()
+print(f"Perimeter: {perimeter} units")
+print(f"Area: {area} square units ")
+
+
+
+
